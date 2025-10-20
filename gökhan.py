83b435c87a961349aeddab52a971e03bb4ffa329
gokhankural <kuralgokhan65@gmail.com>
2025-10-20 12:31:46 +0300

    Add files via upload

---
+++ gökhan.py
@@ -0,0 +1,89 @@
+metin = "merhaba dünya"
+print(metin)
+
+isim = " gökhan"
+print (isim[0:3])
+
+metin = "pyhton prohlamlama dili" 
+
+print(metin)
+print(metin.lower())
+print(metin.upper())
+
+#Bu kod ekrana "hello world" yazar.
+print("hello world")
+
+# Bu kod ekrana "otuz beş" yazar.
+print("35")
+
+gökhan = " selam gökhan nasılsın?"
+print(type(gökhan))
+
+if 489 > 100:
+    print("doğru")
+
+a = 70
+a = 80
+print(a)
+
+sayi41, sayi42, sayi43 = 10, 20, 30
+print(sayi41)
+print(sayi42)
+print(sayi43)
+
+mesaj = "hello world"
+yas = 23
+mesaj_2 = "merhaba dünya"
+print("mesaj, yas, mesaj_2")
+
+sayi1 = 23
+sayi2 = 55.5
+sayi3 = float(sayi1)
+sayi4 = int(sayi2)
+
+print(sayi1)
+print(sayi2)
+print(sayi3)
+print(sayi4)
+
+metin = " hello python"
+
+print(metin)
+print(metin.replace("1" , "b"))
+
+mesaj = "gökhan vanlı."
+
+print("van" in mesaj)
+
+yas = 23
+metin = f"ben {yas} yasındayım"
+print(metin)
+
+yas = 35
+fiyat = f"Bunun fiyatı{yas:.2f}tl'dir."
+print(fiyat)
+
+x, y = "araba", "motor"
+print(x + " ve "  + y)
+
+a = 95
+e = 105
+print(a + e)
+
+s = z = 31
+print(s, z)
+
+bool_deger  = True
+print(type(bool_deger))
+
+x = 2547.65
+print(int(x))
+
+a = 77
+b = 77
+c = 77 
+a = b = c = 77
+
+print(a)
+print(b)
+print(c)
